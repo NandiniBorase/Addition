2376c597465abf5361f251e2fd6f0ab70374e4b8
NandiniBorase <borase.nandini@gmail.com>
2023-02-28 12:35:41 +0530

    success

---
+++ src/com/test/Addition.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20, result;
+
+		result = a + b;
+
+		System.out.println("Addition of two numbers are " + result);
+	}
+
+}
